b406b45184f825932db226a0adf50c937a17066c
emmanuel <sunartio.emmanuel@gmail.com>
2023-02-05 15:10:58 +1030

    finalised SubjectsFragment functionalities

--- app/src/main/java/com/example/timetable/settings/SubjectsFragment.kt
+++ app/src/main/java/com/example/timetable/settings/SubjectsFragment.kt
@@ -2,19 +2,20 @@ package com.example.timetable.settings
 
 import android.os.Bundle
 import android.view.*
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.timetable.R
 import com.example.timetable.Subject
 import com.example.timetable.ViewModel
 import com.example.timetable.databinding.FragmentSubjectsBinding
+import com.google.android.material.snackbar.Snackbar
 
 class SubjectsFragment : Fragment() {
 
@@ -22,7 +23,8 @@ class SubjectsFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var viewModel: ViewModel
-    private lateinit var subjects: ArrayList<Subject>
+    private lateinit var subjects: ArrayList<Subject> // note: don't add or remove items from "subjects" except when user clicks save
+    private lateinit var shownSubjects: ArrayList<Subject>
     private lateinit var colors: List<Int>
     private lateinit var rvAdapter: RVAdapterSubjects
 
@@ -34,6 +36,8 @@ class SubjectsFragment : Fragment() {
             viewModel.createSubjects()
         }
         subjects = viewModel.subjects!!
+        shownSubjects = arrayListOf() // keep track of the subjects currently shown
+        shownSubjects.addAll(subjects)
 
         if (viewModel.colors == null) {
             viewModel.createColors()
@@ -78,24 +82,8 @@ class SubjectsFragment : Fragment() {
     }
 
     private fun saveSubjects() {
-        var n = 0
-        val updatedSubjects = arrayListOf<Subject>()
-
-        while (n < rvAdapter.itemCount) { // for each itemView in RecyclerView
-            val itemView: View = binding.rvSubjects.findViewWithTag(n)
-            val name = itemView.findViewById<EditText>(R.id.etSubject).text.toString().trim()
-            if (name != "") { // only save if its name isn't blank
-                val colorIndex = itemView.findViewById<Spinner>(R.id.spinnerColor).selectedItemPosition
-                val color = colors[colorIndex]
-                val room = itemView.findViewById<EditText>(R.id.etRoom).text.toString().trim()
-
-                val subject = Subject(name, color, room)
-                updatedSubjects.add(subject)
-            }
-            n++
-        }
-
-        viewModel.saveSubjects(updatedSubjects)
+        val shownSubjects = getShownSubjects()
+        viewModel.saveSubjects(shownSubjects)
         Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
 
         requireActivity().findNavController(R.id.navHostFragment).navigate(R.id.action_subjectsFragment_to_settingsFragment) // go back to SettingsFragment
@@ -107,30 +95,88 @@ class SubjectsFragment : Fragment() {
         }
         rvAdapter = RVAdapterSubjects(subjects, colors)
         binding.rvSubjects.adapter = rvAdapter
+
+        // swipe functions
+        ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val shownSubjects = getShownSubjects()
+                val pos = viewHolder.adapterPosition
+                val removedSubject: Subject = shownSubjects[pos]
+
+                // the following was done, instead of simply removing from "subjects", so "subjects" remains unchanged until user clicks save
+                shownSubjects.removeAt(pos)
+                rvAdapter.updateSubjects(shownSubjects) // pass new list to rvAdapter
+                createSnackBar(removedSubject)
+            }
+        }).attachToRecyclerView(binding.rvSubjects)
+    }
+
+    private fun createSnackBar(removedSubject: Subject) { // shows a SnackBar for user to undo removing the subject
+        val snack = Snackbar.make(binding.rvSubjects, "", Snackbar.LENGTH_LONG)
+        val customView = layoutInflater.inflate(R.layout.custom_snackbar, binding.root, false)
+        if (snack.view.background != null) {
+            snack.view.setBackgroundColor(ContextCompat.getColor(requireContext(), com.google.android.material.R.color.mtrl_btn_transparent_bg_color))
+        }
+
+        val snackBarLayout: Snackbar.SnackbarLayout = snack.view as Snackbar.SnackbarLayout
+        snackBarLayout.setPadding(5, 0, 5, 15)
+        snackBarLayout.addView(customView)
+
+        val tvPrimary: TextView = snackBarLayout.findViewById(R.id.tvPrimary)
+        val name = removedSubject.name
+        tvPrimary.text = "$name removed"
+
+        val btnUndo: Button = snackBarLayout.findViewById(R.id.btnAction)
+        btnUndo.text = "undo"
+        btnUndo.setOnClickListener {
+            cancelDelete(removedSubject)
+            snack.dismiss()
+        }
+
+        snack.show()
+    }
+
+    private fun cancelDelete(restoredSubject: Subject) {
+        val updatedSubjects = arrayListOf<Subject>()
+        updatedSubjects.addAll(subjects)
+        updatedSubjects.add(restoredSubject)
+
+        rvAdapter.updateSubjects(updatedSubjects)
     }
 
     private fun addNewSubject() {
         if (rvAdapter.enableAddSubject) { // only add new subject if the last subject's name is not blank
             val newSubject = Subject("", ContextCompat.getColor(requireContext(), R.color.subject_blue), "")
-            val updatedSubjects = arrayListOf<Subject>()
-
-            var n = 0
-            while (n < rvAdapter.itemCount) { // for each itemView in RecyclerView
-                val itemView: View = binding.rvSubjects.findViewWithTag(n)
-                val name = itemView.findViewById<EditText>(R.id.etSubject).text.toString().trim()
-                if (name != "") { // only save if its name isn't blank
-                    val colorIndex = itemView.findViewById<Spinner>(R.id.spinnerColor).selectedItemPosition
-                    val color = colors[colorIndex]
-                    val room = itemView.findViewById<EditText>(R.id.etRoom).text.toString().trim()
-
-                    val subject = Subject(name, color, room)
-                    updatedSubjects.add(subject)
-                }
-                n++
-            }
-
+            val updatedSubjects = getShownSubjects()
             updatedSubjects.add(newSubject)
             rvAdapter.updateSubjects(updatedSubjects)
         }
+    }
+
+    private fun getShownSubjects(): ArrayList<Subject> {
+        val shownSubjects = arrayListOf<Subject>()
+        for (i in 0 until rvAdapter.itemCount) { // for each itemView in RecyclerView
+            val holder = binding.rvSubjects.findViewHolderForAdapterPosition(i)
+            if (holder != null) {
+                val name = holder.itemView.findViewById<EditText>(R.id.etSubject).text.toString().trim()
+                if (name != "") { // only save if its name isn't blank
+                    val colorIndex = holder.itemView.findViewById<Spinner>(R.id.spinnerColor).selectedItemPosition
+                    val color = colors[colorIndex]
+                    val room = holder.itemView.findViewById<EditText>(R.id.etRoom).text.toString().trim()
+
+                    val subject = Subject(name, color, room)
+                    shownSubjects.add(subject)
+                }
+            }
+        }
+        return shownSubjects
     }
 }
